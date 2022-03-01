42b921549dc2f8e59f7c7fc840a0df25c7fd58ad
lara_wood <laragaleeva@gmail.com>
2022-03-01 22:54:46 +0500

    change preloader, fetch

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,17 +1,7 @@
 export function Preloader() {
     return (
-        <div className="preloader-wrapper active">
-            <div className="spinner-layer spinner-red-only">
-                <div className="circle-clipper left">
-                    <div className="circle"/>
-                </div>
-                <div className="gap-patch">
-                    <div className="circle"/>
-                </div>
-                <div className="circle-clipper right">
-                    <div className="circle"/>
-                </div>
-            </div>
+        <div className="progress">
+            <div className="indeterminate"></div>
         </div>
     )
 }
